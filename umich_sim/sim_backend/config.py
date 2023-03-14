8dab1509a005b20ab071f2e1f5c6750998e26bb9
Yuxuan <liuyuxuan131@outlook.com>
2023-03-14 04:46:08 -0400

    Updated config.py based on the feedback

--- umich_sim/sim_backend/config.py
+++ umich_sim/sim_backend/config.py
@@ -1,9 +1,14 @@
-# Python Version: 3.9
+"""
+Implementation of the configuration class according to "Configuration Class Proposal"
+https://docs.google.com/document/d/12WPOGTSmqdKhLk_f6ysbAbLGbMhUN15yMLg7rpNrrJY/
+"""
 
 from typing import *
 from enum import Enum
+from dataclasses import dataclass
 
 # Enums (Auto-assign integer values starting with 1)
+# Environment
 Task = Enum('Task', ['INTERSECTION', 'EXPRESSWAY'])
 Weather = Enum('Weather', ['SUNNY', 'CLOUDY', 'RAINY', 'SNOWY'])
 Scene = Enum('Scene', ['DAY', 'NIGHT'])
@@ -11,83 +16,104 @@ TrafficLightColor = Enum('TrafficLightColor', ['GREEN', 'YELLOW', 'RED'])
 TrafficLightTrigger = Enum('TrafficLightTrigger', ['TIME', 'DISTANCE'])
 Direction = Enum('Direction', ['STRAIGHT', 'LEFT', 'RIGHT'])
 Accident = Enum('Accident', ['NORMAL_STOP, ABRUPT_STOP', 'SLOW_INTRUDE', 'RUN_RED_LIGHT'])
-VehicleType = Enum('VehicleType', ['LEAD', 'AHEAD_LEAD', 'ADJACENT_LEAD', 'FOLLOW', 'SPEED_BARRIER'])
+FreewayVehicleType = Enum('FreewayVehicleType', ['LEAD', 'AHEAD_LEAD', 'ADJACENT_LEAD', 'FOLLOW', 'SPEED_BARRIER'])
 FreewayVehicleBehavior = Enum('FreewayVehicleBehavior', ['KEEP_SAME_SPEED', 'AC_OR_DECELERATE', 'CHANGE_LANES', 'PULL_OFF_STOP', 'ENTER_ADJACENT'])
-VehicleColor = Enum('VehicleColor', []) # to be specified
-BehaviorRoute = Enum('BehaviorRoute', []) # to be specified
+# Vehicle
+VehicleType = Enum('VehicleType', [])  # to be specified
+VehicleColor = Enum('VehicleColor', [])  # to be specified
+TrafficRule = Enum('TrafficRule', ['FOLLOW', 'INTRUDE', 'IGNORE'])
+Route = Enum('Route', ['CHANGE_LANE', 'TURN_AT_INTERSECTION'])  # more to be specified
 
-# BASE CLASSES
-class Position:
-    def __init__(self):
-        # To be specified
-        pass
+@dataclass
+class Location:
+    # To be specified
+    pass
 
-# Environment -> intersection_settings (list of Intersection) -> Intersection -> traffic_light
+# Environment -> intersections (list of Intersection) -> Intersection -> traffic_light
+@dataclass
 class TrafficLight:
-    def __init__(self):
-        self.state_durations: dict[TrafficLightColor, float] = {}
-        self.trigger_type: TrafficLightTrigger = None
-        self.trigger_val: float = 0.0
+    state_durations: Dict[TrafficLightColor, float]
+    trigger_type: TrafficLightTrigger
+    trigger_val: float
 
-# Environment -> intersection_settings (list of Intersection) -> Intersection -> vehicles (list of IntersectionVehicle)
+# Environment -> intersections (list of Intersection) -> Intersection -> vehicles (list of IntersectionVehicle)
+@dataclass
 class IntersectionVehicle:
-    def __init__(self):
-        self.init_position: Position = None
-        self.model: str = ''
-        self.direction: Direction = None
-        self.accident: Accident = None
+    init_location: Location
+    model: str
+    direction: Direction
+    accident: Accident
 
-# Environment -> intersection_settings (list of Intersection)
+# Environment -> intersections (list of Intersection)
+@dataclass
 class Intersection:
-    def __init__(self):
-        self.traffic_light: TrafficLight = None
-        self.vehicles: list[IntersectionVehicle] = []
+    traffic_light: TrafficLight
+    # based on the front-end review
+    subject_lane_vehicles: List[IntersectionVehicle]
+    left_lane_vehicles: List[IntersectionVehicle]
+    right_lane_vehicles: List[IntersectionVehicle]
+    ahead_lane_vehicles: List[IntersectionVehicle]
 
-# Environment -> freeway_settings (list of Freeway) -> Freeway -> vehicles (list of FreewayVehicle)
-class FreewayVehicle:
-    def __init__(self):
-        self.init_position: Position = None
-        self.type: VehicleType = None
-        self.behavior: FreewayVehicleBehavior = None
-        self.ol_random_traffic: bool = False # Opposing Lanes Random Traffic
+# Environment -> freeways (list of Freeway) -> Freeway -> vehicles (list of FreewayVehicle)
+@dataclass
+class FreewayVehicles:
+    init_location: Location
+    type: FreewayVehicleType
+    behavior: FreewayVehicleBehavior
 
-# Environment -> freeway_settings (list of Freeway)
+# Environment -> freeways (list of Freeway) -> Freeway -> vehicles (list of FreewayVehicle) -> FreewayVehicle -> lane_sections (list of LaneSection)
+@dataclass
+class LaneSection:
+    left_lane_vehicles: List[FreewayVehicles]
+    subject_lane_vehicles: List[FreewayVehicles]
+
+# Environment -> freeways (list of Freeway)
+@dataclass
 class Freeway:
-    def __init__(self):
-        self.speed_limit: float = 0.0
-        self.vehicles: list[FreewayVehicle] = []
+    speed_limit: float
+    # based on the front-end review
+    lane_sections: List[LaneSection]
+    ol_random_traffic: bool  # Opposing Lanes Random Traffic
 
 # Environment
+@dataclass
 class Environment:
-    def __init__(self):
-        self.map: str = ''
-        self.task_type: Task = None
-        self.weather: Weather = None
-        self.scene: Scene = None
-        self.num_intersections: int = 0
-        self.intersection_settings: list[Intersection] = []
-        self.freeway_settings: list[Freeway] = []
+    map: str
+    task_type: Task
+    weather: Weather
+    scene: Scene
+    num_intersections: int
+    intersections: List[Intersection]
+    freeways: List[Freeway]
+    # based on the front-end review
+    allow_collision: bool
+    max_speed: float
+    min_speed: float
+    safety_distance: float
 
 # vehicles (list of Vehicle) -> Vehicle -> VehicleBehavior
+@dataclass
 class VehicleBehavior:
-    def __init__(self):
-        self.is_ego: bool = False
-        self.is_leading: bool = False
-        self.is_following: bool = False
-        self.speed: int = 0
-        self.acceleration: int = 0
-        self.follow_traffic_rules: bool = False
-        self.route: BehaviorRoute = None
+    is_ego: bool
+    is_lead_or_follow: bool
+    speed: float
+    acceleration: float
+    traffic_rule: TrafficRule
+    route: Route
 
 # vehicles (list of Vehicle) -> Vehicle
+@dataclass
 class Vehicle:
-    def __init__(self):
-        self.init_position: Position = None
-        self.type: VehicleType = None
-        self.color: VehicleColor = None
-        self.behavior: VehicleBehavior = None # VehicleBehavior is a class, not an Enum
+    init_location: Location
+    type: VehicleType
+    color: VehicleColor
+    behavior: VehicleBehavior  # VehicleBehavior is a class, not an Enum
+    # based on the front-end review
+    gap: float
+    model: str
 
-class Config:
-    def __init__(self, env_in: Environment, vehicles_in: list[Vehicle]):
-        self.env: Environment = env_in
-        self.vehicles: list[Vehicle] = vehicles_in
+# BASE CLASS
+@dataclass
+class ScenarioConfig:
+    env: Environment
+    vehicles: List[Vehicle]
