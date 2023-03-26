0205cb71710df0bf273036e18d6d39fbb0270393
cc <cctom@umich.edu>
2023-03-26 14:54:03 -0400

    add sample config instance from original dict

--- scripts/freeway_test.py
+++ scripts/freeway_test.py
@@ -7,37 +7,58 @@ from hydra.conf import ConfigStore
 from omegaconf import OmegaConf
 from umich_sim.sim_config import ConfigPool, Config, WizardConfig
 import pygame
+from umich_sim.sim_backend.config import *
 
 # Sample configuration dictionary
 # Notes: the vehicle with ID 0 must always be the ego vehicle,
 # the vehicle's ID's must increase in consecutive order, otherwise later vehicles will be left out,
 # the value of spawn_point corresponds with the spawn_point numbers found in the MapExplorationExperiment,
 # spawn_offset shifts the spawn point forward or backward by x meters
+configuration_new = ScenarioConfig(task=Task.FREEWAY)
+configuration_new.freeways.append(
+    Freeway(
+        subject_lane_vehicles=list(
+            Vehicle(
+                location=0,
+                gap=0,
+                model="",
+                color=(255, 255, 255),
+                exp_settings=VehicleExpSettingsFreeway(
+                    vehicle_type=VehicleType.EGO_FULL_MANUAL
+                ),
+            ),
+            Vehicle(
+                location=1,
+                gap=5.0,
+                model="",
+                color=(0, 0, 0),
+                speed=50,
+                exp_settings=VehicleExpSettingsFreeway(
+                    vehicle_type=VehicleType.LEAD, behavior=Behavior.KEEP_SAME_SPEED
+                ),
+            ),
+        )
+    )
+)
 configuration_dictionary = {
     "debug": True,
     "number_of_vehicles": 2,
-
     # Ego vehicle that simply goes straight through each Freeway section
     0: {
         "type": VehicleType.EGO_FULL_MANUAL,
         "spawn_point": 13,
         "spawn_offset": -10.0,
         "initial_lane_index": 1,
-        "sections": {
-            0: 'straight'
-        }
+        "sections": {0: "straight"},
     },
-
     # Initial lead vehicle that turns right at the second intersection
     1: {
         "type": VehicleType.LEAD,
         "spawn_point": 13,
         "spawn_offset": 0.0,
         "initial_lane_index": 1,
-        "sections": {
-            0: 'straight'
-        }
-    }
+        "sections": {0: "straight"},
+    },
 }
 
 
